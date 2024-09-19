814229a22755f0803a01d986c4fe57df929eeec3
StarostinaVS <starostinavs@zabgu.ru>
2024-09-19 16:08:59 +0900

    create models users & users authentication

--- app/settings.py
+++ app/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -125,3 +125,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "users.User"
+
--- users/views.py
+++ users/views.py
@@ -1,9 +1,30 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+
+# from django.contrib.auth import LoginView
+
+# class LoginView(LoginView):
+#     pass
 
 
 def login(request):
-    context = {
-        "title": "Авторизация"
-    }
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid:
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+        else:
+            form = UserLoginForm()
+
+    context = {"title": "Авторизация"}
 
     return render(request, "user/login.html", context=context)
